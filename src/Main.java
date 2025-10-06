dd4a8e1a6ae1be69f81ac7f4d2618ac2df2ca57d
Guardo06 <guardoconeojesusguardo@gmail.com>
2025-10-06 10:21:24 -0500

    Sincronizacion Inicial

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+
+import Formularios.FrmMatriz2;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        FrmMatriz2 F = new FrmMatriz2(3);
+        F.setVisible(true);
+    }
+    
+}
